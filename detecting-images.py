6028e7503fe7caf843c2d8aca7c4d7a6c41d5f4f
João Assalim <joassalim@gmail.com>
2024-01-10 10:48:32 -0300

    Add files via upload

--- detecting-images.py
+++ detecting-images.py
@@ -1,20 +1,33 @@
 import cv2
-from yolov8 import YOLOv8
-import pywt
+from ultralytics import YOLO
 
 def detect_objects_in_photo(image_path):
-    image = cv2.imread(image_path)
-    yolo_model = YOLOv8("./models/best.onnx", conf_thres=0.5, iou_thres=0.5)
+    image_orig = cv2.imread(image_path)
+    
+    yolo_model = YOLO('./runs/detect/Normal_Compressed/weights/best.pt')
+    
+    results = yolo_model(image_orig)
 
-    boxes, scores, class_ids = yolo_model(image)
-    result_image = yolo_model.draw_detections(image)
-    result_path = "./imgs/NoWave/knife1.jpg"
-    cv2.imwrite(result_path, result_image)
+    for result in results:
+        classes = result.names
+        cls = result.boxes.cls
+        conf = result.boxes.conf
+        detections = result.boxes.xyxy
 
+        for pos, detection in enumerate(detections):
+            if conf[pos] >= 0.5:
+                xmin, ymin, xmax, ymax = detection
+                label = f"{classes[int(cls[pos])]} {conf[pos]:.2f}" 
+                color = (0, int(cls[pos]), 255)
+                cv2.rectangle(image_orig, (int(xmin), int(ymin)), (int(xmax), int(ymax)), color, 2)
+                cv2.putText(image_orig, label, (int(xmin), int(ymin) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1, cv2.LINE_AA)
+
+    result_path = "./imgs/Test/teste.jpg"
+    cv2.imwrite(result_path, image_orig)
     return result_path
 
 def detect_objects_in_video(video_path):
-    yolo_model = YOLOv8("./models/best.onnx", conf_thres=0.3, iou_thres=0.5)
+    yolo_model = YOLO('./runs/detect/Normal_Compressed/weights/best.pt')
     video_capture = cv2.VideoCapture(video_path)
     width = int(video_capture.get(3))
     height = int(video_capture.get(4))
@@ -26,32 +39,49 @@ def detect_objects_in_video(video_path):
         ret, frame = video_capture.read()
         if not ret:
             break
-        boxes, scores, class_ids = yolo_model(frame)
-        result_frame = yolo_model.draw_detections(frame)
-        out.write(result_frame)
+        results = yolo_model(frame)
+
+        for result in results:
+            classes = result.names
+            cls = result.boxes.cls
+            conf = result.boxes.conf
+            detections = result.boxes.xyxy
+
+            for pos, detection in enumerate(detections):
+                if conf[pos] >= 0.5:
+                    xmin, ymin, xmax, ymax = detection
+                    label = f"{classes[int(cls[pos])]} {conf[pos]:.2f}" 
+                    color = (0, int(cls[pos]), 255)
+                    cv2.rectangle(frame, (int(xmin), int(ymin)), (int(xmax), int(ymax)), color, 2)
+                    cv2.putText(frame, label, (int(xmin), int(ymin) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1, cv2.LINE_AA)
+
+        out.write(frame)
     video_capture.release()
     out.release()
 
     return result_video_path
 
-photo_path = "./imgs/knife.jpg"
-detect_objects_in_photo(photo_path)
-
-
-def detect_objects_in_photo_wavelets(image_path, path_orig):
-    image = cv2.imread(image_path)
-    image = cv2.resize(image, (640, 640))
+def detect_objects_and_plot(path_orig):
     image_orig = cv2.imread(path_orig)
-    yolo_model = YOLOv8("./models/best-wave.onnx", conf_thres=0.5, iou_thres=0.5)
+    
+    yolo_model = YOLO('./runs/detect/Normal_Compressed/weights/best.pt')
+    
+    results = yolo_model(image_orig)
 
-    boxes, scores, class_ids = yolo_model(image)
-    print(boxes, scores, class_ids)
-    result_image = yolo_model.draw_detections(image_orig)
-    result_path = "./imgs/Wave/knife4.jpg"
-    cv2.imwrite(result_path, result_image)
+    for result in results:
+        classes = result.names
+        cls = result.boxes.cls
+        conf = result.boxes.conf
+        detections = result.boxes.xyxy
 
-    return result_path
-
-photo_path_wave = "./imgs/pre/knife4.jpg"
-photo_path_orig = "./imgs/knife4.jpg"
-detect_objects_in_photo_wavelets(photo_path_wave, photo_path_orig)
+        for pos, detection in enumerate(detections):
+            if conf[pos] >= 0.5:
+                xmin, ymin, xmax, ymax = detection
+                label = f"{classes[int(cls[pos])]} {conf[pos]:.2f}" 
+                color = (0, int(cls[pos]), 255)
+                cv2.rectangle(image_orig, (int(xmin), int(ymin)), (int(xmax), int(ymax)), color, 2)
+                cv2.putText(image_orig, label, (int(xmin), int(ymin) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1, cv2.LINE_AA)
+    
+    cv2.imshow("Teste", image_orig)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
--- preprocessing-images.py
+++ preprocessing-images.py
@@ -1,11 +1,26 @@
 import os
 import cv2
 import pywt
+import numpy as np
 
-def apply_wavelet_transform(image):
-    coeffs2 = pywt.dwt2(image, 'bior1.3')
+def apply_symlet_transform(image):
+    coeffs2 = pywt.dwt2(image, 'sym2')
     LL, (LH, HL, HH) = coeffs2
     return LH
+
+def apply_daubechies_transform(image):
+    coeffs2 = pywt.dwt2(image, 'db2')
+    LL, (LH, HL, HH) = coeffs2
+    return LH
+
+def apply_haar_transform(image):
+    coeffs2 = pywt.dwt2(image, 'haar')
+    LL, (LH, HL, HH) = coeffs2
+    return LH
+
+def enhance_contrast(image):
+    normalized_image = ((image - np.min(image)) / (np.max(image) - np.min(image))) * 255
+    return normalized_image.astype(np.uint8)
 
 def process_images_in_folder(input_folder, output_folder):
     os.makedirs(output_folder, exist_ok=True)
@@ -13,13 +28,16 @@ def process_images_in_folder(input_folder, output_folder):
 
     for image_file in image_files:
         input_path = os.path.join(input_folder, image_file)
-        original = cv2.imread(input_path, cv2.IMREAD_GRAYSCALE)
-        horizontal_detail = apply_wavelet_transform(original)
-        output_path = os.path.join(output_folder, image_file)
-        cv2.imwrite(output_path, horizontal_detail)
+        original = cv2.imread(input_path)
+        horizontal_detail = apply_haar_transform(original)
+        horizontal_detail = enhance_contrast(horizontal_detail)
+        horizontal_detail = cv2.resize(horizontal_detail, (640, 640))
+
+        output_path = os.path.join(output_folder, f'{image_file}')
+        cv2.imwrite(output_path, original, [int(cv2.IMWRITE_JPEG_QUALITY), 20])
         print(f"Detalhe horizontal da transformada wavelet salvo em {output_path}")
 
-
-input_folder = "./model/train/images"
-output_folder = "./model/train/images"
+#input_folder = "./assets/Tes"
+input_folder = "./imgs/Normal/Test/images"
+output_folder = "./imgs/Haar_Compressed/Test/images"
 process_images_in_folder(input_folder, output_folder)
